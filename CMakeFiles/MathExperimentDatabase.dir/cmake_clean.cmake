109b14a551c1bc0f99ad68c29fcf6efdf3dfe421
sudo0272 <sudo180272@protonmail.ch>
2020-08-07 21:48:11 +0900

    Fix untracked files

---
+++ CMakeFiles/MathExperimentDatabase.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/MathExperimentDatabase.dir/src/Database.cpp.o"
+  "CMakeFiles/MathExperimentDatabase.dir/src/Row.cpp.o"
+  "CMakeFiles/MathExperimentDatabase.dir/src/main.cpp.o"
+  "MathExperimentDatabase"
+  "MathExperimentDatabase.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/MathExperimentDatabase.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
