dbd645d9fe8745b7615f4d0445316c7c3b0b6952
sudo0272 <sudo180272@protonmail.ch>
2020-08-07 20:05:33 +0900

    Fix to add addRow method to Database

--- include/Database.h
+++ include/Database.h
@@ -1,9 +1,11 @@
 #include <vector>
+#include <string>
 #include "Row.h"
 
 class Database {
 public:
   unsigned int getRow(unsigned int);
+  unsigned int addrow(std::string id, std::string password, std::string nickname);
     
 private:
   std::vector<Row *> table;
