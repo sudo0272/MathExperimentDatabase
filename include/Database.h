91d3e78a5a7c1190f583838b4952c63da8ee3146
sudo0272 <sudo180272@protonmail.ch>
2020-08-07 21:39:36 +0900

    Implement Database.h

--- include/Database.h
+++ include/Database.h
@@ -1,13 +1,22 @@
+#pragma once
+
 #include <vector>
 #include <string>
 #include "Row.h"
 
 class Database {
 public:
-  unsigned int getRow(unsigned int);
-  unsigned int addrow(std::string id, std::string password, std::string nickname);
+  Database();
+  ~Database();
+  Row *getRow(unsigned int);
+  void addRow(std::string, std::string, std::string);
+  void removeRow(unsigned int);
+  unsigned int getCurrentIndex();
     
 private:
   std::vector<Row *> table;
+  unsigned int findRow(unsigned int, unsigned int, unsigned int);
+  unsigned int currentIndex;
+  void increaseCurrentIndex();
 };
 
